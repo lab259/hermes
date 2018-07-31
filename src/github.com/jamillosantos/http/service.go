7ffdfb9eed66dd5004b7dcf35e91a5223007ff6a
J. Santos <jamillo@gmail.com>
2018-07-31 01:13:53 -0300

    Improves the Service interface

--- src/github.com/jamillosantos/http/service.go
+++ src/github.com/jamillosantos/http/service.go
@@ -1,5 +1,9 @@
 package http
 
+import "errors"
+
+var ErrWrongConfigurationInformed = errors.New("wrong configuration informed")
+
 // Service is an abstraction for implementing parts that can be loaded,
 // reloaded, started and stopped inside of the system.
 //
@@ -7,8 +11,23 @@ package http
 // As simple and wide as it could be this directive will provide an defined
 // signature to implement all your resources.
 type Service interface {
-	Load() error
-	Reload() error
+	// Loads the configuration. If successful nil will be returned, otherwise
+	// the error.
+	LoadConfiguration() (interface{}, error)
+
+	// Applies a given configuration object to the service. If successful nil
+	// will be returned, otherwise the error.
+	ApplyConfiguration(interface{}) error
+
+	// Restarts the service. If successful nil will be returned, otherwise the
+	// error.
+	Restart() error
+
+	// Start starts the service. If successful nil will be returned, otherwise
+	// the error.
 	Start() error
+
+	// Stop stops the service. If successful nil will be returned, otherwise the
+	// error.
 	Stop() error
 }

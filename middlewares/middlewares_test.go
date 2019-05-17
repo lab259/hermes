d4790ffb5d6580741cdebaeb3ac2dbd0ce76d662
Felipe Pontes <felipemfpontes@gmail.com>
2019-05-17 08:04:09 -0300

    Fix recoverable middleware not returning Result

---
+++ middlewares/middlewares_test.go
@@ -0,0 +1,28 @@
+package middlewares_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jamillosantos/macchiato"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+)
+
+func TestMiddlewares(t *testing.T) {
+	log.SetOutput(ginkgo.GinkgoWriter)
+	gomega.RegisterFailHandler(ginkgo.Fail)
+
+	description := "Http Test Suite"
+	if os.Getenv("CI") == "" {
+		macchiato.RunSpecs(t, description)
+	} else {
+		reporterOutputDir := "./test-results"
+		os.MkdirAll(reporterOutputDir, os.ModePerm)
+		junitReporter := reporters.NewJUnitReporter("./test-results/http.xml")
+		macchiatoReporter := macchiato.NewReporter()
+		ginkgo.RunSpecsWithCustomReporters(t, description, []ginkgo.Reporter{macchiatoReporter, junitReporter})
+	}
+}
---
+++ middlewares/recoverable_middleware_test.go
@@ -0,0 +1,35 @@
+package middlewares_test
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/lab259/http"
+	"github.com/lab259/http/middlewares"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/valyala/fasthttp"
+)
+
+var _ = Describe("Http", func() {
+	Describe("Application", func() {
+		It("should recover from panic", func() {
+			r := http.DefaultRouter()
+			r.Use(middlewares.RecoverableMiddleware)
+			r.Get("/should-panic", func(req http.Request, res http.Response) http.Result {
+				panic("The SDD alarm is down, bypass the redundant firewall so we can calculate the IB matrix!")
+			})
+
+			ctx := &fasthttp.RequestCtx{}
+			ctx.Request.Header.SetMethod("GET")
+			ctx.Request.URI().SetPath("/should-panic")
+
+			r.Handler()(ctx)
+
+			tmp := bytes.NewBufferString("")
+			ctx.Response.BodyWriteTo(tmp)
+			Expect(ctx.Response.StatusCode()).To(Equal(fasthttp.StatusInternalServerError))
+			Expect(strings.TrimSpace(tmp.String())).To(Equal(`{"code":"internal-server-error","message":"We encountered an internal error or misconfiguration and was unable to complete your request."}`))
+		})
+	})
+})
